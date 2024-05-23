d80df8e86c837f9aed61ea313000e2b3b9fc5997
Pawel.Piasecki <250988@edu.p.lodz.pl>
2024-05-23 19:51:35 +0200

    Added array and dynamic allocation

---
+++ array.h
@@ -0,0 +1,17 @@
+//
+// Created by pawel on 23.05.2024.
+//
+
+#ifndef DYNAMIC_ALLOCATION_IN_ARRAY_ARRAY_H
+#define DYNAMIC_ALLOCATION_IN_ARRAY_ARRAY_H
+int* enterValues(int *size);
+
+void displayArray(int *array, int size);
+
+void findMaxValue(int *array, int size);
+
+void findMinValue(int *array, int size);
+
+void calculateAverageValue(int *array, int size);
+
+#endif //DYNAMIC_ALLOCATION_IN_ARRAY_ARRAY_H
